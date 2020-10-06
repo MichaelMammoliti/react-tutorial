6432f4ad8364997da2a2e84cc9b3bf645a52c2f0
Michael Mammoliti <mammoliti.michael@gmail.com>
2020-10-06 12:17:25 +0100

    added: divide, subtract and multiply functions

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const add = (num1, num2) => {
   return num1 + num2;
 }
 
+const multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+}
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+}
+
 console.log(add(1, 2));
