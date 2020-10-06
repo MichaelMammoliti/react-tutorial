49cf32d1c5b1c44a3f7fc419dbb4c339acaae6d1
Michael Mammoliti <mammoliti.michael@gmail.com>
2020-10-06 12:18:44 +0100

    created a name space calculator for all our operations

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const add = (num1, num2) => {
-  return num1 + num2;
+
+
+const calculator = {
+  add: (num1, num2) => {
+    return num1 + num2;
+  },
+  multiply: (num1, num2) => {
+    return num1 * num2;
+  },
+  subtract: (num1, num2) => {
+    return num1 - num2;
+  },
+  divide: (num1, num2) => {
+    return num1 / num2;
+  }
 }
 
-const multiply = (num1, num2) => {
-  return num1 * num2;
-}
-
-const subtract = (num1, num2) => {
-  return num1 - num2;
-}
-
-const divide = (num1, num2) => {
-  return num1 / num2;
-}
-
-console.log(add(1, 2));
+console.log(calculator.add(1, 2));
